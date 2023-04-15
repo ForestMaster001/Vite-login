81a1fc866f281ab703ce11e0767fc270e5167c4b
ForestMaster001 <41067317+ForestMaster001@users.noreply.github.com>
2023-04-16 03:20:56 +0900

    first-push

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: process.env.GITHUB_PAGES  // この行を追加
+  ? "Vite-login"            // この行を追加
+  : "./",  
+  plugins: [react(), viteSingleFile()],
+  build:{
+    outDir: "server/hosting",
+  }
+})
