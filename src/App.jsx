c821bc66a820b3c627c7800b12c5796de6784969
ForestMaster001 <41067317+ForestMaster001@users.noreply.github.com>
2023-04-16 03:58:42 +0900

    route svg change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+//import reactLogo from './assets/react.svg'
+//import reactLogo from '/react-35ef61ed.svg'
 import './App.css'
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="./assets/react-35ef61ed.svg" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
